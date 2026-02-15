519e253f9d9e754f95fe179389350643e6d7ef0b
Takeshi Yaeda <t.yaeda@gmail.com>
2026-02-15 13:40:22 +0900

    build: enable vite-plugin-qrcode

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { qrcode } from "vite-plugin-qrcode";
 
 export default defineConfig({
   base: "/kokon/",
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), qrcode(), tailwindcss()],
   test: {
     environment: "jsdom",
     globals: true
